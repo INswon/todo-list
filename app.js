f054c41c1d07e5422ddc9033a4948a4acd506609
practice-ca <rika26887@gmail.com>
2024-08-31 20:58:27 +0900

    Initial commit

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // 初期化処理: ページ読み込み時にローカルストレージからメモを読み込む
 document.addEventListener('DOMContentLoaded', () => {
     memos = JSON.parse(localStorage.getItem('memos')) || [];
@@ -8,23 +7,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
 // メモを保存するための空のリスト
 let memos = []; // グローバルなメモ配列
-let selectedIndex = null; // 現在選択されているメモのインデックスを保存する変数
-
-function addMemo() {
-    // メモを追加
-    const titleInput = document.getElementById('titleInput').value;
-    const memoInput = document.getElementById('memoInput').value;
-
-    if (titleInput && memoInput) {
-        const currentDateTime = new Date().toLocaleString();
-        const memo = { title: titleInput, content: memoInput, date: currentDateTime };
-        memos.push(memo);
-
-        // ローカルストレージに保存
-        localStorage.setItem('memos', JSON.stringify(memos));
-=======
-// メモを保存するための空のリスト
-const memos = [];
 let selectedIndex = null; // 現在選択されているメモのインデックスを保存する変数
 
 function addMemo() {
@@ -38,8 +20,11 @@ function addMemo() {
         // 日時を取得
         const currentDateTime = new Date().toLocaleString();
         // メモをリストに追加
-        memos.push({ title: titleInput, content: memoInput, date: currentDateTime });
->>>>>>> b5e406f1ce46cdd32d48ef0930d2660a550d414a
+        const memo = { title: titleInput, content: memoInput, date: currentDateTime };
+        memos.push(memo);
+        
+        // メモをローカルストレージに保存
+        localStorage.setItem('memos', JSON.stringify(memos));
 
         // 入力フィールドをクリア
         document.getElementById('titleInput').value = '';
@@ -50,22 +35,18 @@ function addMemo() {
     }
 }
 
+
 function updateTitleList() {
     // メモリストを表示する場所(HTML要素)を取得する
     const titleList = document.getElementById('titleList');
     // メモの内容を一度空にする
     titleList.innerHTML = '';
 
-<<<<<<< HEAD
     // ローカルストレージからメモを取得
     const storedMemos = JSON.parse(localStorage.getItem('memos')) || [];
     console.log('Memos to Display:', storedMemos);
 
     storedMemos.forEach((memo, index) => {
-=======
-    // 保存された全てのメモに対して、一つずつ処理を行います。
-    memos.forEach((memo, index) => {
->>>>>>> b5e406f1ce46cdd32d48ef0930d2660a550d414a
         const memoElement = document.createElement('div');
         memoElement.textContent = `${memo.title} - ${memo.content} - ${memo.date}`; // タイトル、内容、日時を一行で表示
 
@@ -83,17 +64,13 @@ function updateTitleList() {
 
 function deleteMemo() {
     if (selectedIndex !== null) {
-<<<<<<< HEAD
         memos.splice(selectedIndex, 1);
         selectedIndex = null;
         localStorage.setItem('memos', JSON.stringify(memos));
         updateTitleList();
-=======
-        memos.splice(selectedIndex, 1); // 選択されたインデックスのメモを削除
-        selectedIndex = null; // インデックスをリセット
-        updateTitleList(); // リストを更新して画面に反映
->>>>>>> b5e406f1ce46cdd32d48ef0930d2660a550d414a
     } else {
         alert('削除するメモを選択してください。');
     }
 }
+
+
